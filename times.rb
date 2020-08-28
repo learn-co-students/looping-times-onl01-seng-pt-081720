842bb145c39f5144c9a3e888cfa4ada0ac03cac4
pleasant-communicator-3558 <sujchand2@gmail.com>
2020-08-28 03:10:22 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,6 +1,9 @@
 7.times do
   puts "Wingardium Leviosa"
- end
-end
+  jewels_in_bag = jewels_in_bag - 10 
+  puts "Now there are only #{jewels_in_bag} jewels left to hide!"
+  end
+  puts "We have#{jewels_in_bag} jewels still to hide!"
+  
 
 
